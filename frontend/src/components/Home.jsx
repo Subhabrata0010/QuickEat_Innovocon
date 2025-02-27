dfde6810454607e5c8d6bbbedc4273946f7aba81
Sowdarjya <kolaysowdarjya@gmail.com>
2025-02-27 22:08:22 +0530

    Auth frontend 🤝 auth backend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function S() {
-  return (
-    <div></div>
-  )
+function Home() {
+  return <div>Welcome to the Home Page!</div>;
 }
 
-export default S
+export default Home;
